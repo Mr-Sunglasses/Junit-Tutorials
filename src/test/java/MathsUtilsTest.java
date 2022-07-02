74d1ceb9e6c6ab49ded8a154ca97ed0f017334b0
Kanishk Pachauri <krishna03012003@icloud.com>
2022-07-03 00:11:06 +0530

    Added tests for add, sub, mul, and div

--- src/test/java/MathsUtilsTest.java
+++ src/test/java/MathsUtilsTest.java
@@ -6,10 +6,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MathsUtilsTest {
 
     @Test
-    void test(){
+    void test_add(){
+        MathsUtils mathsUtils = new MathsUtils();
+        int expected = 23;
+        int actual = mathsUtils.add(11,12);
+        assertEquals(expected,actual,"This test is for add two number a , b");
+
+
+    }
+
+    @Test
+    void test_sub(){
         MathsUtils mathsUtils = new MathsUtils();
         int expected = 22;
-        int actual = mathsUtils.add(11,11);
-        assertEquals(expected, actual);
+        int actual_sub = mathsUtils.sub(33,11);
+        assertEquals(expected,actual_sub);
     }
+
 }
